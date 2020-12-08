701ab69e79924b8a324c57aeeb28a5cafd51221b
root <rafael.tanaka@jetstack.io>
2020-12-08 17:00:19 +0000

    changed manifests names

--- kip1/labelit/setup.sh
+++ kip1/labelit/setup.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
-kubectl apply -f /root/manifests/manifests.yaml
-echo "# Please create your service manifest in this file" > /root/myservice.yaml
+while [ `kubectl get nodes | grep " Ready" |  wc -l` -ne 2 ]
+do
+  sleep 1
+done
+
+cat <<EOF > setup.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: front-end
+  labels:
+    color: client-go
+spec:
+  containers:
+  - image: raftx/sample-client:label-svc
+    name: client-go
+
+---
+
+apiVersion: v1
+kind: Pod
+metadata:
+  name: back-end
+  labels:
+    app: back-end
+spec:
+  containers:
+  - image: raftx/sample-web:v4
+    name: back-end
+    ports:
+    - containerPort: 8080
+EOF
+kubectl apply -f setup.yaml
 
 
 
