2c2d88c528e77a74d79d69a5cdc107b8259e434a
root <rafael.tanaka@jetstack.io>
2020-12-16 08:37:22 +0000

    added break script

---
+++ kipday1/deployment/break.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sample-web:green has a sleep of 5 minutes to simulate a breakage in the app
+kubectl set image deployment color-deployment web-server=raftx/sample-web:green --record=true
