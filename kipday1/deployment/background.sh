46eafd7ce76cdce7dd3f4214cf9535b88b2a0b23
root <rafael.tanaka@jetstack.io>
2020-12-16 10:04:16 +0000

    step1

--- kipday1/deployment/background.sh
+++ kipday1/deployment/background.sh
@@ -1,17 +1,18 @@
+# Loop to wait for both nodes to be in the Ready state before proceeding.
 while [ `kubectl get nodes | grep " Ready" |  wc -l` -ne 2 ]
 do
   sleep 1
 done
 
-FILE=/root/manifests/deploy1.yaml
-
+# Loop to wait for Katacoda to upload the assets files
+FILE=/root/manifests/color-deployment.yaml
 while [ ! -f "$FILE" ]
 do
   sleep 1
 done
 
-kubectl apply -f /root/manifests/deploy1.yaml
+kubectl apply -f /root/manifests/color-deployment.yaml
 
 sleep 2
-
+# Purposefully scaling the deployment to cause a configuration drift.
 kubectl scale --replicas=5 deployment color-deployment
