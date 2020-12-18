ecff8d9d14c4826e7c9ee796e81ef9e32927b8b3
root <rafael.tanaka@jetstack.io>
2020-12-18 09:10:56 +0000

    migrated all scnerios from my personal repo

---
+++ kipday1/labelit/background.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+while [ `kubectl get nodes | grep " Ready" |  wc -l` -ne 2 ]
+do
+  sleep 1
+done
+
+cat <<EOF > setup.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: front-end
+  labels:
+    color: client-go
+spec:
+  containers:
+  - image: raftx/sample-client:label-svc
+    name: client-go
+
+---
+
+apiVersion: v1
+kind: Pod
+metadata:
+  name: back-end
+  labels:
+    app: back-end
+spec:
+  containers:
+  - image: raftx/sample-web:v4
+    name: back-end
+    ports:
+    - containerPort: 8080
+EOF
+kubectl apply -f setup.yaml
+touch /root/myservice.yaml
+
+
+
